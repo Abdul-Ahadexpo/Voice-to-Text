1a497786e169170d850271283664eabc9b7d5dc4
Abdul-Ahadexpo <adahadd4845e@gmail.com>
2024-06-22 23:13:51 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const startBtn = document.getElementById('start-btn');
-const stopBtn = document.getElementById('stop-btn');
-const outputTextBox = document.getElementById('output-text-box');
-const container = document.querySelector('.container');
-let recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
-recognition.lang = 'en-US'; // Default language
+const startBtn = document.getElementById("start-btn");
+const stopBtn = document.getElementById("stop-btn");
+const outputTextBox = document.getElementById("output-text-box");
+const container = document.querySelector(".container");
+const languageSelect = document.getElementById("language-select");
+let recognition = new (window.SpeechRecognition ||
+  window.webkitSpeechRecognition)();
+recognition.lang = "en-US";
 recognition.interimResults = true;
 
-startBtn.addEventListener('click', () => {
-  recognition.lang = 'en-US'; // Set to English by default
+startBtn.addEventListener("click", () => {
+  recognition.lang = languageSelect.value; // Set the selected language
   startRecognition();
 });
 
-stopBtn.addEventListener('click', () => {
+stopBtn.addEventListener("click", () => {
   recognition.stop();
   toggleButtons(false);
 });
@@ -19,17 +21,17 @@ stopBtn.addEventListener('click', () => {
 function startRecognition() {
   recognition.start();
   toggleButtons(true);
-  outputTextBox.value = 'Listening...';
+  outputTextBox.value = "Listening...";
 }
 
 function toggleButtons(isListening) {
   startBtn.disabled = isListening;
   stopBtn.disabled = !isListening;
-  container.classList.toggle('listening', isListening);
+  container.classList.toggle("listening", isListening);
 }
 
 recognition.onresult = (event) => {
-  let transcript = '';
+  let transcript = "";
   for (let i = event.resultIndex; i < event.results.length; i++) {
     transcript += event.results[i][0].transcript;
   }
@@ -37,19 +39,18 @@ recognition.onresult = (event) => {
 };
 
 recognition.onerror = (event) => {
-  outputTextBox.value = 'Error occurred in recognition: ' + event.error;
+  outputTextBox.value = "Error occurred in recognition: " + event.error;
   toggleButtons(false);
 };
 
 recognition.onend = () => {
-  if (!startBtn.disabled) {
-    outputTextBox.value += ' Stopped.';
+  if (startBtn.disabled) {
     toggleButtons(false);
   }
 };
 
 // Additional function to switch recognition language to Bengali (Bangla)
 function switchToBangla() {
-  recognition.lang = 'bn-BD';
+  recognition.lang = "bn-BD";
   startRecognition();
 }
